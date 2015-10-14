45d9424db4fabfde2c810690f1053e9bdf7d9db4
Ian Palmgren <ianmpal@gmail.com>
2015-10-14 16:54:09 -0700

    Midway Push

--- domain-modeling/main.swift
+++ domain-modeling/main.swift
@@ -8,5 +8,65 @@
 
 import Foundation
 
-print("Hello, World!")
+
+struct Job {
+    var title: String
+    var salaryHourly: Int
+    var slarlyYearly: Int
+    
+    func calculateIncome(hoursWorked: Int) -> String {
+        
+    }
+
+struct Spouse {
+    var name: String
+    
+}
+
+
+struct Person {
+    let firstName : String
+    let lastName : String
+    var age : Int
+    var job : Job?
+    var spouse: Spouse?
+  
+    
+    init (firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+    
+    init (firstName: String, lastName: String, age: Int, job: Job, spouse: Spouse) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.job = job
+        self.spouse = spouse
+    }
+    
+    
+    func toString() -> String {
+        if age < 16 {
+            return "Name: \(firstName) \(lastName) \nAge: \(age) \nJob: US Citizens under 16 are not allowed to work \nSpouse: US Citizens under 18 are not allowed to get married)"
+        } else if age > 16 && age < 18 {
+            return "Name: \(firstName) \(lastName) \nAge: \(age) \nJob: \(job!) \nSpouse: Not over 18"
+        } else {
+            return "Name: \(firstName) \(lastName) \nAge: \(age) \nJob: \(job!) \nSpouse: \(spouse!)"
+        }
+    }
+    
+   
+}
+
+var job1 = Job(title: "contractor")
+var sp1 = Spouse(name: "margaret")
+var p1 = Person (firstName: "Ian", lastName: "Palmgren", age: 19, job: job1, spouse: sp1)
+//var p2 = Person (firstName: "Ian", lastName: "Palmgren", age: 24, job: Job, spouse: Spouse)
+
+
+print(p1.toString())
+
+
 
