af49b7e3db9fc590dba1286b6c8db6833d042b62
Ian Palmgren <ianmpal@gmail.com>
2015-10-14 15:27:17 -0700

    first commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Ian Palmgren on 10/14/15.
+//  Copyright © 2015 Ian Palmgren. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
